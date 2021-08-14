cef5f9beb294b1922fc0cb72ba29c21405fe478d
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-08-14 21:11:09 +0530

    LinkedList Added

--- ArrayList/Intro.java
+++ ArrayList/Intro.java
@@ -1,10 +1,38 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class Intro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
+		ArrayList<Integer> arr =new ArrayList<>();
+		arr.add(10);
+		arr.add(20);
+		arr.add(30); //adds element to last
+		System.out.println(arr.size());
+		arr.add(1, 50); //1 at which index to add & 50 is value
+		System.out.println(arr.get(1));   //to get element of particular index
+		arr.set(0, 100); //will set 100 value to index 0
+		
+		
+		
+		
+//		iterating over ArrayList
+for(int i=0;i<arr.size();i++) {
+	System.out.print(arr.get(i)+" ");
+}		
+//	for each loop
+System.out.println();
+System.out.println("For Each Loop");
+for(int i:arr) {
+	System.out.print(i+" ");
+	
+	}
+		
+		
+		
 	}
 
 }
---
+++ LinkedList/CountIndex.java
@@ -0,0 +1,55 @@
+package LinkedList;
+
+public class CountIndex {
+
+	public static SizeLL<Integer> countSizeInput() {
+		
+		SizeLL<Integer> l1=new  SizeLL<>(10);
+		SizeLL<Integer> l2= new SizeLL<>(20);
+		SizeLL<Integer> l3= new SizeLL<>(30);
+		SizeLL<Integer> l4= new SizeLL<>(40);
+		SizeLL<Integer> l5= new SizeLL<>(50);	
+		l1.next=l2;
+		l2.next=l3;
+		l3.next=l4;
+		l4.next=l5;
+		return l1;	
+	}
+	
+		
+public static int print(SizeLL<Integer> head,int pos) {
+	SizeLL<Integer> temp = head;
+	int i = 0;
+	while(i<pos-1) {
+		temp=temp.next;
+		i++;
+	}
+	temp=temp.next;
+	int ith=temp.data;
+	return ith;
+}
+	
+//OR (Optimized)
+public static void printIndex(SizeLL<Integer> head,int i) {
+	int pos = 0;
+	SizeLL<Integer> temp = head;
+	while(temp != null && pos<i ) {
+		temp = temp.next;
+		pos++;
+	}
+	if(temp !=null) {
+		System.out.println( "Data is "+ temp.data);
+	}
+}
+	
+	public static void main(String[] args) {
+		
+	SizeLL<Integer> size = countSizeInput();
+//	int ans = 	print(size, 2);
+//	System.out.println(ans);
+	int ans = print(size,2);
+	System.out.println(ans);
+	
+	}
+
+}
