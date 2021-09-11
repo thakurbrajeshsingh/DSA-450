2070896c6ea6d26395404259f523a47ed40803f8
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-09-12 00:35:41 +0530

    Dyanamic Queues

--- Queue/QueueUsingArray.java
+++ Queue/QueueUsingArray.java
@@ -31,7 +31,7 @@ public class QueueUsingArray {
 		
 	public void enqueue(int elem) throws QueueFullException {
 		if(size == data.length) {
-			throw new QueueFullException();
+			doubleCapacity();
 		}
 		if(size == 0) {
 			front=0;
@@ -45,6 +45,21 @@ public class QueueUsingArray {
 		}
 	
 		
+	private void doubleCapacity() {
+		int[] temp =data;
+		data = new int[2*temp.length];
+		int index=0;
+		for(int i= front; i<temp.length;i++) {
+			data[index++]=temp[i];
+		}
+		for(int i=0; i<front-1;i++) {
+			data[index++]=temp[i];
+		}
+		front = 0;
+		rear = temp.length-1;
+	}
+	
+	
 	public int front() throws QueueEmptyException {
 		if(size == 0) {
 			throw new QueueEmptyException();
